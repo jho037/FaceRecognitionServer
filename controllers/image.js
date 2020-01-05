97be7400f9f80de3eedd1934046165614252ebf4
Johnny <jho037@ucr.edu>
2020-01-05 15:10:38 -0800

    First commit

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = requrie('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '4b4e203c09584fcdb9a5aee92f8e8fa6'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to use API'))
+}
+
+
+const handleImage = (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries=> {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
+
